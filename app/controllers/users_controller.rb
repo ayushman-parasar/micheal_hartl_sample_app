f568735f4b31430613da52683c06aee9e585d1e3
ayushman-parasar <learnertsk@gmail.com>
2020-08-10 20:54:22 +0530

    password nil allowed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "User Updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+
+  end
+
   
 
   private
