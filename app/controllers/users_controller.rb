42a92b001da5d4391734adf7a9a7f6ef3c0ec7f9
ayushman-parasar <learnertsk@gmail.com>
2020-08-10 20:11:30 +0530

    remembering user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
+      
       redirect_to @user, notice: "Account created successfully"
     else
       render 'new'
