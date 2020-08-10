d8118c7d1e772eeec760b5801ee42ebf14c9b0e0
ayushman-parasar <learnertsk@gmail.com>
2020-08-10 18:25:26 +0530

    basic login done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-   
+      log_in @user
       redirect_to @user, notice: "Account created successfully"
     else
       render 'new'
     end
   end
+
+  
 
   private
     def user_params
