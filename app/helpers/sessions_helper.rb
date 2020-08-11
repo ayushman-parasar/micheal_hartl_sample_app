fb65308e1132ea36052c212b4d0e7b3e9be4d709
ayushman-parasar <learnertsk@gmail.com>
2020-08-12 01:45:59 +0530

    micropost feed shown

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -61,6 +61,13 @@ module SessionsHelper
     session[:forwarding_url] = request.original_url if request.get?
   end
   
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 
   
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -53,5 +53,13 @@ class UserTest < ActiveSupport::TestCase
     test "authenticated? should return false for a user with nil digest" do
       assert_not @user.authenticated?(:remember,'')
     end
+
+    test "destroy user should destroy micropost" do
+      @user.save
+      @user.microposts.create!(content: "Lorem ipsum")
+      assert_difference 'Micropost.count', -1 do
+        @user.destroy
+      end
+    end
   
 end
