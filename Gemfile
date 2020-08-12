6d22b15b33df437eb46d74397bbba22967ab9c12
ayushman-parasar <learnertsk@gmail.com>
2020-08-12 17:09:29 +0530

    unsafe

--- Gemfile
+++ Gemfile
@@ -42,7 +42,7 @@ gem 'image_processing', '1.9.3'
 
 gem 'mini_magick', '4.9.5'
 
-gem 'cloudinary'
+
 
 
 group :development, :test do
--- config/initializers/cloudinary.rb
+++
@@ -1,7 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = 'dmgzj7gru'
-  config.api_key = '579686583728188'
-  config.api_secret = 'WQj0YHKyUdL4O_hfwHBil7tkctc'
-  config.secure = true
-  config.cdn_subdomain = true
-end
